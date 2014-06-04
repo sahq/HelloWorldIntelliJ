26cc1bc96c955964da48b08ebf811042cffbfb45
unknown <jykim@WW731702C45645.cnb.Corp.net>
2014-06-03 18:06:28 -0700

    Initial commit

---
+++ src/us/sahq/Main.java
@@ -0,0 +1,10 @@
+package us.sahq;
+
+public class Main {
+    static int a = 1;
+    static int b = 2;
+    public static void main(String[] args)
+    {
+	    System.out.println("hello! "+a);
+    }
+}
